6d2c3d173f6cc407cbb7f9115cabce174317376c
Patrick O'Neill <synapse.and.syntax@gmail.com>
2013-06-30 16:31:51 -0400

    first working clojure prototype

--- src/clojure_shootout/src/clojure_shootout/core.clj
+++ src/clojure_shootout/src/clojure_shootout/core.clj
@@ -5,7 +5,7 @@
 (defn parse-genome
   "Return genome as string"
   []; no arguments
-  (let [genome-file "../../data/NC_000913.fna"]
+  (let [genome-file "../../data/NC000913.fna"]
     (->> genome-file
          slurp
          (drop-while (fn [c] (not (= c \newline))))
@@ -41,6 +41,11 @@
          ]
      (apply zipmap (transpose list-comp#))))
 
+(def ln2 (Math/log 2))
+
+(defn log2 [x]
+  (/ (Math/log x) ln2))
+
 (defn make-pssm
   "make PSSM from binding sites"
   [binding-sites]
@@ -48,18 +53,32 @@
         n (count binding-sites)]
     (for [col cols]
       (hash-for [b "ACGT"]
-                [b (/ (count (filter (fn [x] (= x b)) col))
-                      n)]))))
+                [b (log2 (/ (float (/ (+ (count (filter (fn [x] (= x b)) col)) 1)
+                                      (+ n 4)))
+                            0.25))]))))
 
 (defn sum [xs]
   (apply + xs))
 
+
 (defn score
   "Score site with pssm"
-  [pssm, site]
+  [pssm site]
   (sum (for [[col-hash c] (zip pssm site)] (col-hash c))))
+
+(defn slide-score
+  "Score genome by pssm via sliding-window"
+  [pssm genome]
+  (let [width (count pssm)]
+    (for [i (range (+ (count genome) (- width) 1))]
+      (score pssm (subs genome i (+ i width))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [genome (parse-genome)
+        binding-sites (parse-binding-sites)
+        pssm (make-pssm binding-sites)
+        scores (slide-score pssm genome)]
+    (spit "shootout_clojure_results.txt"
+          (clojure.string/join "\n" scores))))
