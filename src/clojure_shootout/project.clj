b85f6f2c8364b6d3ee7b7bf7794b5def51daecb6
Patrick O'Neill <synapse.and.syntax@gmail.com>
2013-06-30 17:25:17 -0400

    cleaned up project.clj a bit

--- src/clojure_shootout/project.clj
+++ src/clojure_shootout/project.clj
@@ -1,6 +1,6 @@
 (defproject clojure_shootout "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure code for PSSM shootout"
+  :url "compbio.umbc.edu/pat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
